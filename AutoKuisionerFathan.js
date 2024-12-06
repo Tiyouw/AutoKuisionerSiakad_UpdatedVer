6d696508aba681dc4a8ef41250fce366140d28b8
Ahimsa Jenar Bramsaifstyo Kusuma <90629154+Tiyouw@users.noreply.github.com>
2024-12-06 12:49:53 +0700

    Update AutoKuisionerFathan.js
    FIx backtick in fetch and console.log

--- AutoKuisionerFathan.js
+++ AutoKuisionerFathan.js
@@ -3,7 +3,7 @@ async function kuis(matkul, dosen) {
     const response = await fetch(`https://siakad.unej.ac.id/Kuesioner/Inputkuesioner?idmatakuliah=${matkul}&iddosen=${dosen}`, {
       method: "POST",
       headers: {
-        accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,id;q=0.7",
         "cache-control": "max-age=0",
         "content-type": "application/x-www-form-urlencoded",
